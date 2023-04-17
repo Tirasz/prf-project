dfd9ea3125e3f6f4bab80612327c12d13e5e4c6a
Tirasz <zacsi143@gmail.com>
2023-04-17 16:43:52 +0200

    init backend

---
+++ be-garfield-torrent/dist/environment.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = void 0;
+exports.environment = {
+    CONNECTION_STRING: "mongodb+srv://rozsa_balazs99:L5TrrXzSjSxVl5wh@prfcluster.fd9efho.mongodb.net/?retryWrites=true&w=majority"
+};
