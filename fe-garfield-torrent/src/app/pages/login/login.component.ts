b0aa3ef223d02a38203b729742e834969119e3b9
Tirasz <zacsi143@gmail.com>
2023-04-18 12:52:00 +0200

    Login skeleton

--- fe-garfield-torrent/src/app/pages/login/login.component.ts
+++ fe-garfield-torrent/src/app/pages/login/login.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  isLoading = new BehaviorSubject(false);
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.minLength(6))
+  })
+
+  constructor(
+    private location: Location,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    console.log('LOGGING IN WITH: ', email, password)
+    this.isLoading.next(true);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
