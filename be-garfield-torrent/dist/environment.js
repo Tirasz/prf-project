4d60acfb163660327d51fb9455dbcf97d7041fe7
Tirasz <zacsi143@gmail.com>
2023-04-18 13:16:22 +0200

    what

--- be-garfield-torrent/dist/environment.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    CONNECTION_STRING: "mongodb+srv://rozsa_balazs99:L5TrrXzSjSxVl5wh@prfcluster.fd9efho.mongodb.net/?retryWrites=true&w=majority"
-};
