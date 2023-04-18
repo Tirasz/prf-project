0ed959ba18730d43e6cc5eafed712ace408c3993
Tirasz <zacsi143@gmail.com>
2023-04-18 18:02:25 +0200

    Moved create torrent page to correct fodler

--- fe-garfield-torrent/src/app/app-routing.module.ts
+++ fe-garfield-torrent/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) }, { path: 'create-torrent', loadChildren: () => import('./shared/pages/create-torrent/create-torrent.module').then(m => m.CreateTorrentModule) }];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) }, { path: 'create-torrent', loadChildren: () => import('./pages/create-torrent/create-torrent.module').then(m => m.CreateTorrentModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
