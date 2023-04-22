8b7269e187f5a8e37cfb097e1d822ac0d287c6b1
Tirasz <zacsi143@gmail.com>
2023-04-22 15:23:26 +0200

    Torrent browser

--- fe-garfield-torrent/src/app/pages/browser/browser.component.ts
+++ fe-garfield-torrent/src/app/pages/browser/browser.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { TorrentService } from '../../shared/services/Torrent/torrent.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Torrent } from '../../shared/models/Torrent';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-browser',
   templateUrl: './browser.component.html',
   styleUrls: ['./browser.component.scss']
 })
-export class BrowserComponent {
+export class BrowserComponent implements OnInit, AfterViewInit {
+
+  columnNames: string[] = ['title', 'category', 'created', 'owner', 'seeders', 'leechers']
+  dataSource = new MatTableDataSource<Torrent>();
+
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  constructor(
+    private torrentService: TorrentService,
+    private router: Router
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.torrentService.getAllTorrents().subscribe(torrents => {
+      this.dataSource.data = torrents;
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator
+    })
+  }
+
+  ngOnInit(): void { }
+
+  onRowClicked(row: any) {
+    this.router.navigate(['/torrent', row.id]);
+  }
 
 }
--- fe-garfield-torrent/src/app/pages/browser/browser.module.ts
+++ fe-garfield-torrent/src/app/pages/browser/browser.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BrowserRoutingModule } from './browser-routing.module';
 import { BrowserComponent } from './browser.component';
-
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,11 @@ import { BrowserComponent } from './browser.component';
   ],
   imports: [
     CommonModule,
-    BrowserRoutingModule
+    BrowserRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ]
 })
 export class BrowserModule { }
